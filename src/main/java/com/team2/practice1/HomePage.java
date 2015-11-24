05a95e2a63d1847c98371fb44662ce5501c971e5
Bruce Han <1494292038@qq.com>
2015-11-24 13:48:39 +0800

    bruce version 1

---
+++ src/main/java/com/team2/practice1/HomePage.java
@@ -0,0 +1,15 @@
+package com.team2.practice1;
+
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage {
+		
+	public HomePage() {
+		
+	}
+	
+	public void selectType() {
+		
+	}
+	
+}
