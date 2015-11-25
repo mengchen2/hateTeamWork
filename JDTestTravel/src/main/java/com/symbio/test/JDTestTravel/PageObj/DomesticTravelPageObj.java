5d7a98502ed66468cbdc7f90ef4024d85513e79c
Mousumi <mousumi.sen@symbio.com>
2015-11-25 13:51:26 +0800

    Domestic & Login Done..

--- JDTestTravel/src/main/java/com/symbio/test/JDTestTravel/PageObj/DomesticTravelPageObj.java
+++ JDTestTravel/src/main/java/com/symbio/test/JDTestTravel/PageObj/DomesticTravelPageObj.java
@@ -2,17 +2,98 @@ package com.symbio.test.JDTestTravel.PageObj;
 
 import java.util.Date;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+import com.symbio.test.JDTestTravel.helper.ElementUtil;
+
 public class DomesticTravelPageObj {
 
+	private WebDriver driver;
+	private ElementUtil eUtil;
+	
+	@FindBy(css = "#fore1")
+	private WebElement domesticTravelTab;
+	
+	@FindBy(css = "#depCity")
+	private WebElement startCityTextField;
+
+	@FindBy(css = "#arrCity")
+	private WebElement destinationCityTextField;
+	
+	@FindBy(css = "#roundFlight")
+	private WebElement travelTypeRadioButton;
+	
+	@FindBy(css = "#depDate")
+	private WebElement startDatePicker;
+
+	@FindBy(css = "#arrDate")
+	private WebElement endDatePicker;
+	
+	@FindBy(css = "#validQuery")
+	private WebElement searchFlightButton;
+	
+	
+	/**
+	 * Switch to this page
+	 */
+	public void goToThisPage() {
+		eUtil.switchToNewestWindow();
+	}
+	
+	public void goDomesticPage() {
+		domesticTravelTab.click();
+	}
+	
+	public DomesticTravelPageObj(WebDriver driver) {
+		// Initialize driver
+		this.driver = driver;
+
+		// Set implicit wait of 20 seconds
+		PageFactory.initElements(new AjaxElementLocatorFactory(this.driver, 20), this);
+		eUtil = new ElementUtil(driver);
+	}
+	/**
+	 * Insert place name to both origin and destination textfields
+	 * 
+	 * @param source
+	 *            Origin place name
+	 * @param destination
+	 *            Destination place name
+	 */
+	
 	public void insertDestination(String source, String destination) {
-
+		startCityTextField.sendKeys(source);
+		destinationCityTextField.sendKeys(destination);
 	}
-
+	
+	/**
+	 * Insert start and end date of travel
+	 * 
+	 * @param startDate
+	 * @param endDate
+	 */
+	
 	public void insertDate(Date startDate, Date endDate) {
-
+		startDatePicker.click();
+		startDatePicker.click();
+		// Using util class to pick date in date picker element
+		eUtil.clickDate(startDate);
+		eUtil.clickDate(endDate);
 	}
-
+	
+	/**
+	 * Click the radio button for round travel
+	 */
+	public void pickRoundTravel() {
+		travelTypeRadioButton.click();
+	}
+	
 	public void clickSearchButton() {
-
+		searchFlightButton.click();
 	}
+	
 }
--- JDTestTravel/src/test/java/com/symbio/test/JDTestTravel/TestCase_Domestic.java
+++ JDTestTravel/src/test/java/com/symbio/test/JDTestTravel/TestCase_Domestic.java
@@ -1,12 +1,7 @@
 package com.symbio.test.JDTestTravel;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
@@ -15,10 +10,11 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
+import com.symbio.test.JDTestTravel.PageObj.DomesticTravelPageObj;
 import com.symbio.test.JDTestTravel.PageObj.InternationalTravelPageObj;
 import com.symbio.test.JDTestTravel.PageObj.MainPageObj;
-import com.symbio.test.JDTestTravel.helper.ElementUtil;
 
 public class TestCase_Domestic {
 	
@@ -30,52 +26,34 @@ public class TestCase_Domestic {
 	
 	private Date startDate;
 	private Date endDate;
+	
 	private WebDriver driver;
 	 
 	private MainPageObj mainPO;
-	private InternationalTravelPageObj travelPO; 
+	private DomesticTravelPageObj travelPO; 
 	
 	@Test(groups = "jdtravel-test")
-	public void test1() {
-		driver = new FirefoxDriver();
-		driver.get("http://jipiao.jd.com/");
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		driver.findElement(By.id("fore2")).click();
-		driver.findElement(By.id("gjdepDate")).click(); 
-		ElementUtil eUtil = new ElementUtil(driver); 
-		Calendar calendar = new GregorianCalendar();
-		calendar.setTime(new Date());
-		calendar.set(Calendar.DATE, 25); 
-		calendar.set(Calendar.MONTH, Calendar.DECEMBER);
-		eUtil.clickDate(calendar.getTime());
-		calendar.set(Calendar.DATE, 4);
-		calendar.set(Calendar.YEAR, 2016); 
-		calendar.set(Calendar.MONTH, Calendar.JANUARY);
-		eUtil.clickDate(calendar.getTime()); 
+	public void test() {
+		mainPO.goToTravel();
+		travelPO.goToThisPage();
+		travelPO.goDomesticPage();
+		travelPO.pickRoundTravel();
+		travelPO.insertDestination(originCity, destinationCity);
+		travelPO.insertDate(startDate, endDate);
+		travelPO.clickSearchButton();
 				
 	}
-	@Test(groups = "jdtravel-test")
-	 public void test() {
-	 mainPO.goToTravel();
-	 //travelPO.goToThisPage();
-	// travelPO.goInternationalPage();
-	// travelPO.pickRoundTravel();
-	 travelPO.insertDestination(originCity, destinationCity);
-	 travelPO.insertDate(startDate, endDate);
-	 travelPO.openAdvancedOption();
-	 travelPO.selectNumberOfAdult(3);
-	 travelPO.clickSearchButton();
-		 } 
 	
 	@BeforeClass(groups = "jdtravel-test")
 	public void initTest() throws ParseException {
 		driver = new FirefoxDriver();
-		mainPO = new MainPageObj(driver); 
-		travelPO = new InternationalTravelPageObj(driver);  
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");  
-		startDate = sdf.parse(startDateString);  
-		endDate = sdf.parse(endDateString); 
+		mainPO = new MainPageObj(driver);
+		travelPO = new DomesticTravelPageObj(driver);
 
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		startDate = sdf.parse(startDateString);
+		endDate = sdf.parse(endDateString);
+		
 	}
 
 	@BeforeMethod(groups = "jdtravel-test")
@@ -85,6 +63,6 @@ public class TestCase_Domestic {
 
 	@AfterClass(groups = "jdtravel-test")
 	public void destroyTest() {
-		//driver.quit();
+		driver.quit();
 	}
 }
